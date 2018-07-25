914cadbbedc17b27b8c3d2115a8b50527765d8b1
raul-sanz <38414010+raul-sanz@users.noreply.github.com>
2018-07-24 19:31:07 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,3 @@
 export default{
-    apiKey: "AIzaSyCFbURGorYCFpU8jlBzM18fLIaI0s-ycMc",
-    authDomain: "pagina-de-prueba-35c6f.firebaseapp.com",
-    databaseURL: "https://pagina-de-prueba-35c6f.firebaseio.com",
-    projectId: "pagina-de-prueba-35c6f",
-    storageBucket: "pagina-de-prueba-35c6f.appspot.com",
-    messagingSenderId: "1060857269288"
+    // aqui ba tu informacion privada de firebase solo copia y pega
 }
